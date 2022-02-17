2f5ce3e1387722ef698d36b3c2cc429b385b8714
Coy1944 <Samuelchincks86@gmail.com>
2022-02-17 09:26:21 -0700

    EOD submit

--- arrayMethods.js
+++ arrayMethods.js
@@ -18,7 +18,9 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 // CODE HERE
-const evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+const evenNumbers = mixedNumbers.filter(function(element) {
+  return element % 2 === 0
+}) // = mixedNumbers.filter(/* Provide Your Callback Here */)
 
 
 
@@ -39,7 +41,9 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 // CODE HERE
-const postTaxPrices // = prices.map(/* Provide Your Callback Here );
+const postTaxPrices  = prices.map(function(element) {
+  return elem * 1.07
+})// = prices.map(/* Provide Your Callback Here );
 
 
 
@@ -57,7 +61,9 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 // CODE HERE
-const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+const totalPopulation = populations.reduce(function(acc, element) {
+  return acc + element
+}) //  = populations.reduce(/* Provide Your Callback Here */)
 
 
 
@@ -82,7 +88,9 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 // CODE HERE
-const myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+const myStrongest = monstersInYourPocket.filter(function(element) {
+  return element.cp > 200 
+})// = monstersInYourPocket.filter(/* Provide Your Callback Here */)
 
 
 
@@ -100,6 +108,10 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
 */
 
 // CODE HERE
+const orderTotals = orders.map(function(element) {
+  return element.price + (element.price * element.tax)
+})
+
 
 
 
@@ -120,3 +132,5 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
 */
 
 // CODE HERE
+const bobsTotal = purchases.filter(purchases => purchases.owner === "Bob")
+.reduce((a,c) => a + c.price, 0)
